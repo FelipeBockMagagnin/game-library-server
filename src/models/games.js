0cdabdb04b2923852f8ae0041eefa12d6791b46b
felipe.magagnin <felipebmagagnin@gmail.com>
2023-05-04 16:13:04 -0300

    feat: end coonnection on select/insert finished

--- src/models/games.js
+++ src/models/games.js
@@ -3,12 +3,14 @@ const db = require("../database/connection");
 async function select(user_id) {
   const client = await db.connect();
   const res = await client.query(`SELECT * FROM games where user_id = '${user_id}'`);
+  await client.end();
   return res.rows;
 }
 
 async function selectByGameID(user_id, game_id) {
   const client = await db.connect();
   const res = await client.query(`SELECT * FROM games where user_id = '${user_id}' and game_id = '${game_id}'`);
+  await client.end();
   return res.rows;
 }
 
@@ -16,7 +18,9 @@ async function insert(data) {
   const client = await db.connect();
   const sql = 'INSERT INTO games(current_status, game_id, user_id) VALUES ($1,$2,$3);';
   const values = [data.current_status, data.game_id, data.user_id];
-  return await client.query(sql, values);
+  const insertReturn = await client.query(sql, values);
+  await client.end();
+  return insertReturn;
 }
 
 module.exports = { select, insert, selectByGameID }
--- src/models/users.js
+++ src/models/users.js
@@ -3,6 +3,7 @@ const db = require("../database/connection");
 async function select(id_google) {
   const client = await db.connect();
   const res = await client.query(`SELECT * FROM users where id_google = '${id_google}'`);
+  await client.end();
   return res.rows;
 }
 
@@ -10,7 +11,9 @@ async function insert(data) {
   const client = await db.connect();
   const sql = 'INSERT INTO users(id_google, email, name) VALUES ($1,$2,$3);';
   const values = [data.id_google, data.email, data.name];
-  return await client.query(sql, values);
+  const query = await client.query(sql, values);
+  await client.end();
+  return query;
 }
 
 module.exports = { select, insert }
