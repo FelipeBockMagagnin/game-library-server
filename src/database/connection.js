408cb767733f215cc327db4c6edd427088173275
felpi <felipebmagagnin#gmail.com>
2023-06-03 19:38:54 -0300

    fix: database not loading on second request

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,16 +1,10 @@
 async function connect() {
   try {
-    //console.log('global connection', global.connection)
-    if (global.connection)
-        return global.connection.connect();
-
     const { Pool } = require('pg');
     const pool = new Pool({
         connectionString: 'postgres://hqkcypqt:fCgNnBjuXupj4H7zXIVkMZdbMODTUlMN@tuffi.db.elephantsql.com/hqkcypqt'
     });
 
-    //guardando para usar sempre o mesmo
-    global.connection = pool;
     return pool.connect();
   } catch (error) {
     console.log('error')
